e1c0aa76e50b561a73981e3a26bb01f820a93c1c
RG9303 <66274568+RG9303@users.noreply.github.com>
2020-06-08 13:56:32 -0700

    Principal code to make a map

---
+++ R/Packages_Install.R
@@ -0,0 +1,21 @@
+
+## If a package is installed, it will be loaded. If any
+## are not, the missing package(s) will be installed
+## from CRAN and then loaded.
+
+## First specify the packages of interest
+packages = c("devtools", "roxygen2", "readr", "tidyverse", "geomorph",
+             "phytools", "viridis", "dplyr", "tidyr", "maps", "graphics")
+
+## Now load or install&load all
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
+
