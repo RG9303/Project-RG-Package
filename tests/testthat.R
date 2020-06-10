b7ec3ecc89ec0881539e59c86627582c89d15630
RG9303 <66274568+RG9303@users.noreply.github.com>
2020-06-10 15:12:11 -0500

    Actualized

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,4 +3,12 @@ library(testthat)
 #
 # test_check("ProjectRGGit")
 
-test_package("ProjectRGGit")
+devtools::test("C:/Users/rubis/Desktop/GitHubR/ProjectRGGit/tests/testthat")
+
+test_that("test number of columes are 50", {
+
+  file = paste0(system.file("vignettes", package = "ProjectRGGit"), "/",make_filename(2013))
+
+  expect_equal(ncol(fars_read(file)), 50)
+
+})
