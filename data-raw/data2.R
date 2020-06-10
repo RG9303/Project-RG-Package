c40c125f6fe52e59422706c331b61cd604918ff5
RG9303 <66274568+RG9303@users.noreply.github.com>
2020-06-10 14:25:37 -0500

    Add files

---
+++ data-raw/data2.R
@@ -0,0 +1,3 @@
+## code to prepare `data2` dataset goes here
+
+usethis::use_data(data2, overwrite = TRUE)
---
+++ data-raw/data3.R
@@ -0,0 +1,3 @@
+## code to prepare `data3` dataset goes here
+
+usethis::use_data(data3, overwrite = TRUE)
